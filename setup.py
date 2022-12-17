3f0fc4d72e84f3f8f652491f873488f3091463eb
KD-7 <91730569+KD-7@users.noreply.github.com>
2022-12-17 21:20:01 +0000

    Update release info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Truelearn Library',
-    version='1.0',
+    version='0.0.1',
     packages=['truelearn_experiments'],
     url='',
     license='MIT',
