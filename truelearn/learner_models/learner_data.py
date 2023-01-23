26e44f3b5c1293911d0bc0ff26bbd31a64cd679d
KD-7 <91730569+KD-7@users.noreply.github.com>
2023-01-23 11:54:24 +0000

    Add methods and attributes

--- truelearn/learner_models/learner_data.py
+++ truelearn/learner_models/learner_data.py
@@ -1,2 +1,18 @@
 class LearnerDataModel:
-    pass
+
+    def __init__(self, learner_id, learner_data, skill=0., variance=0., tau=0., beta_squared=0.):
+        self._learner_id = learner_id
+        self._learner_data = learner_data
+        self._skill = skill
+        self._variance = variance
+        self._tau = tau
+        self._beta_squared = beta_squared
+
+    def get_learner_id(self):
+        return self._learner_id
+
+    def get_learner_data(self):
+        return self._learner_data
+
+    def update_skill(self, skill):
+        self._skill = skill
