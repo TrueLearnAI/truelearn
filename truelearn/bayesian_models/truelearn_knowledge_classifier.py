288ab5e3725705be0db64cae70c4cdb3c945b421
KD-7 <91730569+KD-7@users.noreply.github.com>
2023-01-23 15:15:13 +0000

    add type hints

--- truelearn/bayesian_models/truelearn_knowledge_classifier.py
+++ truelearn/bayesian_models/truelearn_knowledge_classifier.py
@@ -1,20 +1,22 @@
 from truelearn.learner_models.learner_data import LearnerDataModel
+from truelearn.metrics.confusion_matrix import ConfusionMatrix
 
 
 class TrueLearnKnowledgeClassifier:
 
     # Default create a new model
-    def __init__(self, learner_model=LearnerDataModel()):
+    def __init__(self, learner_model=LearnerDataModel()) -> None:
         self._learner_model = learner_model
 
-    def fit(self):
+    def fit(self) -> None:
         pass
 
-    def predict(self):
+    # Binary classification, yes or no (true or false)
+    def predict(self) -> bool:
         pass
 
-    def predict_proba(self):
+    def predict_proba(self) -> float:
         pass
 
-    def score(self):
+    def score(self) -> ConfusionMatrix:
         pass
