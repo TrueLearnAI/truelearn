ac92eb0ab82ba07119507462283e2713976d9e99
KD-7 <91730569+KD-7@users.noreply.github.com>
2023-01-24 22:13:52 +0000

    Change package structure remove unneeded dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name='truelearn',
     # major.minor.patch versioning convention
     version='1.1.1',
-    packages=['truelearn_experiments', 'truelearn.unit_tests', 'truelearn.bayesian_models',
-              'truelearn.preprocessing', 'truelearn.visualisations', 'truelearn.visualisations.plotting_utils'],
+    packages=['truelearn_experiments', 'truelearn.tests', 'truelearn.learning',
+              'truelearn.preprocessing', 'truelearn.learner_models', 'truelearn.metrics',
+              'truelearn.datasets', 'truelearn_logging', 'truelearn_utils'],
     url='',
     license='MIT',
     author='',
@@ -14,16 +15,7 @@ setup(
     install_requires=[
         'numpy>=1.14.1',
         'pandas>=0.22.0',
-        'scipy>=1.0.1',
-        'nltk>=3.2.5',
-        'xmltodict>=0.11.0',
-        'ujson>=1.35',
-        'scikit-learn>=0.19.1',
-        'joblib>=0.14.1',
         'trueskill>=0.4.5',
         'mpmath>=1.1.0',
-        'networkx>=2.5',
-        'statsmodels>=0.12.1',
-        'wordcloud'
     ]
 )
