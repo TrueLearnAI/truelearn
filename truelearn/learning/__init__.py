16918909bab215872c9406c4d8e04cbbbd4c2f38
Yuxiang Qiu <qyxtim@qq.com>
2023-02-11 14:20:17 +0000

    style: apply the line break

--- truelearn/learning/__init__.py
+++ truelearn/learning/__init__.py
@@ -8,5 +8,11 @@ from ._engage_classifier import EngageClassifier
 from ._persistent_classifier import PersistentClassifier
 from ._majority_classifier import MajorityClassifier
 
-__all__ = ["InkClassifier", "InterestClassifier", "KnowledgeClassifier",
-           "NoveltyClassifier", "EngageClassifier", "PersistentClassifier", "MajorityClassifier"]
+__all__ = ["InkClassifier",
+           "InterestClassifier",
+           "KnowledgeClassifier",
+           "NoveltyClassifier",
+           "EngageClassifier",
+           "PersistentClassifier",
+           "MajorityClassifier",
+           ]
