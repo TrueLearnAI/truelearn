504e4a9cd7015e09674c5290be43b6722625bad6
KD-7 <91730569+KD-7@users.noreply.github.com>
2023-02-12 15:43:56 +0000

    remove __init__.py info, as its defined in #16

--- truelearn/__init__.py
+++
@@ -1,9 +0,0 @@
-"""major.minor.patch versioning convention
-==========================================
-major: breaking changes
-minor: new features
-patch: bug fixes
-"""
-__version__ = '1.1.1'
-
-__all__ = ['datasets', 'learning', 'models', 'preprocessing', 'tests', 'utils']
