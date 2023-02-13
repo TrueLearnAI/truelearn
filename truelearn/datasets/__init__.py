a172894faad0e7c53544bc10e3ca281d249d8fe6
Yuxiang Qiu <qyxtim@qq.com>
2023-02-13 22:20:09 +0000

    feat: implement `truelearn.datasets`

---
+++ truelearn/datasets/__init__.py
@@ -0,0 +1,4 @@
+"""The truelearn.datasets implement the methods to load PEEKDataset."""
+from ._peek import load_peek_dataset
+
+__all__ = ["load_peek_dataset"]
---
+++ truelearn/datasets/_base.py
@@ -0,0 +1,110 @@
+from typing import Optional
+from urllib import request
+from os import path
+import hashlib
+import dataclasses
+
+
+# pylint: disable=pointless-string-statement
+"""
+Copyright of `RemoteFileMetaData`, `download_file` are held by
+[BSD 3-Clause License, scikit-learn developers, 2007-2022].
+"""
+
+
+@dataclasses.dataclass
+class RemoteFileMetaData:
+    """Remote file metadata.
+
+    Args:
+        url: The url of the file.
+        filename: The filename of the downloaded file.
+        sha256_expected: The expected sha256 sum of the file.
+    """
+
+    url: str
+    filename: str
+    sha256: str
+
+
+def _sha256sum(filepath):
+    buf_size = 65536
+    h = hashlib.sha256()
+    with open(filepath, "rb", buffering=0) as file:
+        while True:
+            chunk = file.read(buf_size)
+            if not chunk:
+                break
+            h.update(chunk)
+    return h.hexdigest()
+
+
+def download_file(
+    *, remote_file: RemoteFileMetaData, dirname: Optional[str] = None
+) -> str:
+    """Download a remote file and check the sha256.
+
+    Args:
+        remote_file:
+            Some metadata about the remote file.
+        dirname:
+            An optional path that specifies the location of the downloaded file.
+
+    Returns:
+        file_path: Full path of the created file.
+    """
+    filepath = (
+        remote_file.filename
+        if dirname is None
+        else path.join(dirname, remote_file.filename)
+    )
+    if remote_file.url.lower().startswith("http"):
+        print(f"Downloading {remote_file.url} into {filepath}")
+        # the bandit warning is suppressed here
+        # because we have checked whether the url starts with http
+        request.urlretrieve(remote_file.url, filepath)  # nosec
+    else:
+        raise ValueError(
+            f"The given url {remote_file.url} is not a valid http/https url."
+        )
+
+    sha256_actual = _sha256sum(filepath)
+    if remote_file.sha256 != sha256_actual:
+        raise IOError(
+            f"{filepath} has an SHA256 checksum ({sha256_actual}) "
+            f"differing from expected ({remote_file.sha256}), "
+            "file may be corrupted."
+        )
+
+    return filepath
+
+
+def check_and_download_file(
+    *, remote_file: RemoteFileMetaData, dirname: Optional[str] = None
+) -> str:
+    """Download a remote file and check the sha256.
+
+    Args:
+        remote_file:
+            Some metadata about the remote file.
+        dirname:
+            An optional path that specifies the location of the downloaded file.
+
+    Returns:
+        file_path: Full path of the created file.
+    """
+    filepath = (
+        remote_file.filename
+        if dirname is None
+        else path.join(dirname, remote_file.filename)
+    )
+
+    # if already downloaded
+    if (
+        path.exists(filepath)
+        and path.isfile(filepath)
+        and remote_file.sha256 == _sha256sum(filepath)
+    ):
+        return filepath
+
+    return download_file(remote_file=remote_file, dirname=dirname)
