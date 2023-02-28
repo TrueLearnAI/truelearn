f19ba5653d6e50d9ee33d1b99a7ce26e819146fc
KD-7 <91730569+KD-7@users.noreply.github.com>
2023-02-28 10:49:15 +0000

    docs: switch to linkcode

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,11 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
+import inspect
 import sys
+import subprocess
+import re
+from typing import Any
 
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -26,7 +30,7 @@ release = truelearn.__version__
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.viewcode',
+              'sphinx.ext.linkcode',
               'sphinx.ext.autosummary',
               'sphinx.ext.napoleon',
               'sphinx.ext.intersphinx',
@@ -39,6 +43,65 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 autodoc_mock_imports = ['trueskill',
                         'sklearn',
                         'mpmath']
+
+# -- Options for intersphinx extension ---------------------------------------
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'sklearn': ('https://scikit-learn.org/stable/', None),
+    'mpmath': ('https://mpmath.org/doc/current/', None),
+    'trueskill': ('https://trueskill.org/', None),
+}
+
+
+# -- Options for linkcode extension ------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/linkcode.html
+# Code below from:
+# https://github.com/DisnakeDev/disnake/blob/7853da70b13fcd2978c39c0b7efa59b34d298186
+# /docs/conf.py#L192
+
+def git(*args):
+    return subprocess.check_output(["git", *args]).strip().decode()
+
+
+# Current git reference. Uses branch/tag name if found, otherwise uses commit hash
+git_ref = None
+try:
+    git_ref = git("name-rev", "--name-only", "--no-undefined", "HEAD")
+    git_ref = re.sub(r"^(remotes/[^/]+|tags)/", "", git_ref)
+except Exception:
+    pass
+
+# (if no name found or relative ref, use commit hash instead)
+if not git_ref or re.search(r"[\^~]", git_ref):
+    try:
+        git_ref = git("rev-parse", "HEAD")
+    except Exception:
+        git_ref = "main"
+
+
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+
+    try:
+        obj: Any = sys.modules[info["module"]]
+        for part in info["fullname"].split("."):
+            obj = getattr(obj, part)
+        obj = inspect.unwrap(obj)
+
+        if isinstance(obj, property):
+            obj = inspect.unwrap(obj.fget)  # type: ignore
+
+        path = os.path.relpath(inspect.getsourcefile(obj),
+                               start=_disnake_module_path)  # type: ignore
+        src, lineno = inspect.getsourcelines(obj)
+    except Exception:
+        return None
+
+    path = f"{path}#L{lineno}-L{lineno + len(src) - 1}"
+    return f"https://github.com/comp0016-group1/truelearn/blob/{git_ref}/truelearn/" \
+           f"{path}"
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
