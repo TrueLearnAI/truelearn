e82449a03f37993fa620bb2d334e8af6ab7421ed
Aaneel Shalman Srazali <aaneelshalman@192.168.1.182>
2023-03-03 09:34:48 +0000

    Added bar graph plotting

--- truelearn/utils/visualisations/__init__.py
+++ truelearn/utils/visualisations/__init__.py
@@ -3,8 +3,11 @@ output from the classifiers."""
 
 from ._base import knowledge_to_dict
 from ._line_plotter import LinePlotter
+from ._bar_plotter import BarPlotter
+from ._dot_plotter import DotPlotter
 
 __all__ = [
     "knowledge_to_dict",
     "LinePlotter",
+    "BarPlotter"
 ]
