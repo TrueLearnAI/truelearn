c92445d433accac5c2f9ace5a19c629858c37305
Denis Elezi <deniselezi2003@outlook.com>
2023-03-13 16:47:24 +0000

    added PiePlotter to __init__

--- truelearn/utils/visualisations/__init__.py
+++ truelearn/utils/visualisations/__init__.py
@@ -3,6 +3,7 @@ output from the classifiers."""
 
 from ._base import knowledge_to_dict
 from ._line_plotter import LinePlotter
+from ._pie_plotter import PiePlotter
 from ._bar_plotter import BarPlotter
 from ._dot_plotter import DotPlotter
 from ._bubble_plotter import BubblePlotter
@@ -10,6 +11,7 @@ from ._bubble_plotter import BubblePlotter
 __all__ = [
     "knowledge_to_dict",
     "LinePlotter",
+    "PiePlotter",
     "BarPlotter",
     "DotPlotter",
     "BubblePLotter"
