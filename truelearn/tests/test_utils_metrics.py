4ba3ba9c4247abe99b45fa483fbc4b785bdb1471
Yuxiang Qiu <qyxtim@qq.com>
2023-03-22 10:41:44 +0000

    test: add tests for `zero_division`

--- truelearn/tests/test_utils_metrics.py
+++ truelearn/tests/test_utils_metrics.py
@@ -10,10 +10,22 @@ def test_precision_score():
     assert metrics.get_precision_score(y_true, y_pred) == 0.5
 
 
+def test_precision_score_zero_divisions():
+    y_true = [False, True, False, False, True, True]
+    y_pred = [False, False, False, False, False, False]
+    assert metrics.get_precision_score(y_true, y_pred, 0) == 0.0
+
+
 def test_recall_score():
     y_true = [False, True, False, False, True, True]
     y_pred = [False, False, True, False, False, True]
     assert math.isclose(metrics.get_recall_score(y_true, y_pred), 1 / 3)
+
+
+def test_recall_score_zero_division():
+    y_true = [False, False, False, False, False, False]
+    y_pred = [False, False, True, False, False, True]
+    assert metrics.get_recall_score(y_true, y_pred, 0) == 0.0
 
 
 def test_accuracy_score():
@@ -26,3 +38,8 @@ def test_f1_score():
     y_true = [False, True, False, False, True, True]
     y_pred = [False, False, True, False, False, True]
     assert metrics.get_f1_score(y_true, y_pred) == 0.4
+
+def test_f1_score_zero_division():
+    y_true = [False, False, False, False, False, False]
+    y_pred = [False, False, False, False, False, False]
+    assert metrics.get_f1_score(y_true, y_pred, 0) == 0.0
