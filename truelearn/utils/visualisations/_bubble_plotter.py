29d55a20bfc8115dd6193e2de7692f428ca76d00
Aaneel Shalman Srazali <aaneelshalmans@gmail.com>
2023-03-22 19:49:45 +0000

    added plot function

--- truelearn/utils/visualisations/_bubble_plotter.py
+++ truelearn/utils/visualisations/_bubble_plotter.py
@@ -77,3 +77,90 @@ class BubblePlotter(BasePlotter):
 
         return content
 
+    def plot(
+            self,
+            content: Iterable[Tuple[Iterable, Iterable, str]],
+            history: bool,
+            top_n: int = 15,
+            title: str = "Comparison of learner's top 15 subjects",
+            x_label: str = "Mean",
+            y_label: str = "Variances",
+    ) -> go.Scatterpolar:
+
+        """
+        Plots the radar chart using the data.
+
+        Uses content and layout_data to generate a Figure object and stores
+        it into self.figure.
+
+        Args:
+            history: a Boolean value to indicate whether or not the user wants
+              to visualise the history component of the knowledge. If set to 
+              True, number of videos watched by the user and the timestamp of
+              the last video watched by the user will be displayed by the 
+              visualisation hover text.
+            top_n: the number of knowledge components to visualise.
+              e.g. top_n = 5 would visualise the top 5 knowledge components 
+              ranked by mean.
+        """
+        if isinstance(content, Knowledge):
+            content = self._standardise_data(content, history)
+
+
+        content = content[:top_n]
+        
+
+        means = [lst[0]*10 for lst in content]
+
+        variances = [lst[1] for lst in content]
+
+        titles = [lst[2] for lst in content]
+
+        print(titles)
+        print(means)
+        print(variances)
+
+        # compute circle positions:
+        circles = circlify.circlify(
+            means, 
+            show_enclosure=True, 
+            target_enclosure=circlify.Circle(x=0, y=0, r=1)
+        )
+        print("-------------")
+        print(circles)
+
+        # Create just a figure and only one subplot
+        fig, ax = plt.subplots(figsize=(11.75,10))
+
+        # Title
+        ax.set_title(title)
+
+        # Remove axes
+        ax.axis('off')
+
+        # Find axis boundaries
+        lim = max(
+            max(
+                abs(circle.x) + circle.r,
+                abs(circle.y) + circle.r,
+            )
+            for circle in circles
+        )
+        plt.xlim(-lim, lim)
+        plt.ylim(-lim, lim)
+
+        # list of labels
+
+        # Define colormap
+        cmap = cm.get_cmap('Greens_r')
+
+        # Normalize data range to colormap range
+        norm = colors.Normalize(vmin=min(variances) - 0.05, vmax=max(variances) + 0.05)
+
+
+        
+        plt.show()
+        return self
+
+    def _trace(self):
+        pass
