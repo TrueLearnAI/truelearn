730efc7a8cbf7f2dee17dee065b1cae078681df6
Denis Elezi <deniselezi2003@outlook.com>
2023-03-22 10:47:36 +0000

    Made number_of_videos in other slice an average

--- truelearn/utils/visualisations/__init__.py
+++ truelearn/utils/visualisations/__init__.py
@@ -3,8 +3,8 @@ output from the classifiers."""
 
 from ._base import knowledge_to_dict
 from ._line_plotter import LinePlotter
-from ._pie_plotter import PiePlotter
-from ._pie_plotter import RosePlotter
+from ._pie_rose_plotter import PiePlotter
+from ._pie_rose_plotter import RosePlotter
 from ._bar_plotter import BarPlotter
 from ._dot_plotter import DotPlotter
 from ._bubble_plotter import BubblePlotter
@@ -13,6 +13,7 @@ __all__ = [
     "knowledge_to_dict",
     "LinePlotter",
     "PiePlotter",
+    "RosePlotter",
     "BarPlotter",
     "DotPlotter",
     "BubblePlotter"
