77797da6faa9f153b235626bc610be1e94270bc7
Yuxiang Qiu <qyxtim@qq.com>
2023-04-09 16:15:03 +0100

    refactor: update treemap

--- truelearn/utils/visualisations/_treemap_plotter.py
+++ truelearn/utils/visualisations/_treemap_plotter.py
@@ -1,4 +1,4 @@
-from typing import Iterable, List, Optional, Tuple, Union
+from typing import Iterable, Optional
 from typing_extensions import Self
 
 import numpy as np
@@ -9,46 +9,54 @@ from truelearn.utils.visualisations._base import PlotlyBasePlotter
 
 
 class TreePlotter(PlotlyBasePlotter):
-    """Provides utilities for plotting treemaps."""
+    """Treemap Plotter.
+
+    Visualise the learner's knowledge in a treemap.
+    Each subject is represented by a rectangle.
+    The size of the rectangle is determined by the mean.
+    """
+
+    def __init__(
+        self,
+        title: str = "Comparison of learner's subjects",
+        xlabel: str = "",
+        ylabel: str = "",
+    ):
+        """Init a TreeMap plotter.
+
+        Args:
+            title: the default title of the visualization
+            xlabel: the default x label of the visualization
+            ylabel: the default y label of the visualization
+        """
+        super().__init__(title, xlabel, ylabel)
 
     def plot(
         self,
-        content: Union[Knowledge, List[Tuple]],
+        content: Knowledge,
         topics: Optional[Iterable[str]] = None,
         top_n: Optional[int] = None,
-        *,
-        title: str = "Comparison of learner's subjects",
-        x_label: str = "",
-        y_label: str = "",
         history: bool = False,
     ) -> Self:
-        if isinstance(content, Knowledge):
-            content = self._standardise_data(content, history, topics)
+        content_dict = self._standardise_data(content, history, topics)[:top_n]
 
-        content = content[:top_n]
-
-        means = [lst[0] for lst in content]
-
-        variances = [lst[1] for lst in content]
-
-        titles = [lst[2] for lst in content]
+        means, variances, titles, *others = list(zip(*content_dict))
 
         if history:
-            timestamps = [lst[3] for lst in content]
+            timestamps = others[0]
             number_of_videos = []
             last_video_watched = []
             for timestamp in timestamps:
                 number_of_videos.append(len(timestamp))
                 last_video_watched.append(timestamp[-1])
         else:
-            number_of_videos = [None for _ in variances]
-            last_video_watched = [None for _ in variances]
+            number_of_videos = last_video_watched = [None] * len(variances)
 
-        self.figure = go.Figure(
+        self.figure.add_trace(
             go.Treemap(
                 labels=titles,
                 values=means,
-                parents=[""] * len(titles),
+                parents=[None] * len(titles),
                 marker_colors=[
                     "pink",
                     "royalblue",
@@ -78,10 +86,8 @@ class TreePlotter(PlotlyBasePlotter):
                     ),
                     history,
                 ),
-            ),
-            layout=self._layout((title, "", "")),
+            )
         )
-
         self.figure.update_layout(margin={"t": 50, "l": 25, "r": 25, "b": 25})
 
         return self
